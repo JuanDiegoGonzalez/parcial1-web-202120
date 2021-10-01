ef1930f83f43e236736c6942c6aeb1e830a19305
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-10-01 13:45:09 -0500

    Version Pre-final

--- public/script.js
+++ public/script.js
@@ -1,14 +1,12 @@
-const enterButton = document.getElementById('enter');
-const input = document.getElementById('inputText');
-const tableSection = document.getElementById('table-section');
-const table = document.getElementById('table');
-const tbody = document.getElementById('body-table');
-const message = document.getElementById('message');
+const enterButton = document.getElementById("enter");
+const input = document.getElementById("inputText");
+const tableSection = document.getElementById("table-section");
+const table = document.getElementById("table");
+const tbody = document.getElementById("body-table");
+const message = document.getElementById("message");
 
-enterButton.addEventListener('click', (event) => {
-  //Implementar lógica del button submit
-  alert('Implementar lógica del button submit');
-  getresults(123);
+enterButton.addEventListener("click", (event) => {
+  getresults(document.getElementById("inputText").value);
   event.preventDefault();
 });
 
@@ -19,8 +17,23 @@ enterButton.addEventListener('click', (event) => {
 async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
-  console.log('data from back', data);
-  //printValues(data);
+  renderizarRepuesta(data);
+}
+
+function renderizarRepuesta(data) {
+  const tabla = document.getElementById("body-table");
+  removeAllChildNodes(tabla);
+  data.forEach((elem) => {
+    const fila = document.createElement("tr");
+
+    fila.innerHTML = `
+    <th scope="row">${elem.id}</th>
+    <td>${elem.jug1}</td>
+    <td>${elem.jug2}</td>
+    `;
+
+    tabla.appendChild(fila);
+  });
 }
 
 function removeAllChildNodes(parent) {
--- src/controller.js
+++ src/controller.js
@@ -1,8 +1,38 @@
-const { response, request } = require('express');
+var axios = require("axios");
+const { response, request } = require("express");
 
 const getPairsOfPlayers = async (req = request, resp = response, next) => {
-  //Implementar logica aquí
-  return resp.json({ message: 'To be implemented' });
+  const { data } = await axios(
+    `https://gist.githubusercontent.com/jhonatan89/bc554ec3ded15407609714e8b7b8f1c8/raw/5ab1e3e5b45e99aabcbbdd30f55d2ae5eafb9cbe/nba-players`
+  );
+  const results = data.values.map((elem) => ({
+    first_name: elem.first_name,
+    last_name: elem.last_name,
+    h_in: elem.h_in,
+  }));
+
+  let respuesta = [];
+
+  for (let i = 0; i < results.length; i++) {
+    for (let j = i + 1; j < results.length; j++) {
+      alturaJug1 = parseInt(results[i].h_in);
+      alturaJug2 = parseInt(results[j].h_in);
+
+      if (alturaJug1 + alturaJug2 === parseInt(req.query.input)) {
+        nombreJug1 = results[i].first_name + " " + results[i].last_name;
+        nombreJug2 = results[j].first_name + " " + results[j].last_name;
+
+        respuesta.push({
+          id: respuesta.length + 1,
+          jug1: nombreJug1,
+          jug2: nombreJug2,
+        });
+      }
+    }
+  }
+
+  resp.json(respuesta);
+  //return resp.json({ message: "To be implemented" });
 };
 
 module.exports = { getPairsOfPlayers };
