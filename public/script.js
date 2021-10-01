90141fc3f1427d68ddb79d049522d59d490ad38f
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-10-01 13:53:16 -0500

    Version final

--- public/script.js
+++ public/script.js
@@ -17,12 +17,17 @@ enterButton.addEventListener("click", (event) => {
 async function getresults(heightRef) {
   const resp = await fetch(`api?input=${heightRef}`);
   const data = await resp.json();
+
   renderizarRepuesta(data);
+  if (data.length === 0) {
+    alert("No matches found");
+  }
 }
 
 function renderizarRepuesta(data) {
   const tabla = document.getElementById("body-table");
   removeAllChildNodes(tabla);
+
   data.forEach((elem) => {
     const fila = document.createElement("tr");
 
